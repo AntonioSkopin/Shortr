c8b680579a828c0de9c46d17709e2e6db30e9aa5
AntonioSkopin <askopin@outlook.com>
2021-05-21 12:19:17 +0200

    #9 Renamed url routes

--- Routes/urlRoutes.js
+++ Routes/urlRoutes.js
@@ -3,6 +3,6 @@ const urlController = require("../Controllers/urlController");
 
 router.route("/api/urls").get(urlController.getURLs);
 router.route("/api/url").post(urlController.shortenURL);
-router.route("/api/redirect/:urlID").get(urlController.redirectUser);
+router.route("/:urlID").get(urlController.redirectUser);
 
 module.exports = router;
